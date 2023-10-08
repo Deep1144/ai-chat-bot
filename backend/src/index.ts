5665581114993e7748d4f4483ded69ff84fc4c5b
Deep <pateldeep5724@gmail.com>
2023-10-08 16:40:21 +0530

    added logger

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,6 +1,7 @@
 import 'dotenv/config'
 import express, { Application } from "express";
 import cors, { CorsOptions } from "cors";
+import morgan from 'morgan'
 import Routes from "./routes";
 
 export default class Server {
@@ -11,7 +12,7 @@ export default class Server {
 
   private config(app: Application): void {
     const corsOptions: CorsOptions = {};
-
+    app.use(morgan('dev'))
     app.use(cors(corsOptions));
     app.use(express.json());
     app.use(express.urlencoded({ extended: true }));
